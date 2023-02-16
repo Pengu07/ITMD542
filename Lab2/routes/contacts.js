d4a01123f43cbd2b1cbbb3cbbb8d718589aaee1c
Aleks <akamenovic@hawk.iit.edu>
2023-02-16 11:25:50 -0600

    Add Create Contact function and validation

--- Lab2/routes/contacts.js
+++ Lab2/routes/contacts.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var router = express.Router();
 const contactsRepository = require ('../src/contactsMemoryRepository');
-const { body, validationResults } = require ('express-validator');
+const { body, validationResult } = require ('express-validator');
 
 router.get('/', function(req, res, next) {
   const data = contactsRepository.findAll();
-  res.render('contacts', {title: 'Contacts'});
+  res.render('contacts', {title: 'Contacts', contacts: data});
 });
 
 router.get('/create', function(req, res, next) {
     res.render('contacts_create', { title: 'Create a new contact'});
+});
+
+router.post('/create',
+    body('firstName').trim().notEmpty().withMessage('First Name cannot be empty!'),
+    body('lastName').trim().notEmpty().withMessage('Last Name cannot be empty!'),
+    body('email').trim().notEmpty().withMessage('Email cannot be empty!').isEmail().withMessage('Must be a valid email address!'),
+    body('notes').trim(),
+    function(req, res, next) {
+
+    const result = validationResult(req);
+    if (result.isEmpty() != true){
+        res.render('contacts_create', { title: 'Create a new contact', message: result.array() })
+    }
+    else{
+        contactsRepository.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            notes: req.body.notes,
+        });
+
+        res.redirect('/contacts');
+    }
 });
 
 module.exports = router;
--- Lab2/src/contactsMemoryRepository.js
+++ Lab2/src/contactsMemoryRepository.js
@@ -1,6 +1,26 @@
 const database = new Map();
+let numberId = 0;
+
 
 const repository = {
     findAll: () => Array.from(database.values()),
+    create: (contacts) => {
+        const newContact = {
+            id: numberId++,
+            firstName: contacts.firstName,
+            lastName: contacts.lastName,
+            email: contacts.email,
+            notes: contacts.notes,
+            creation: new Date(),
+            /*This can be used if we want to standardize the time across the database
+            if it was used in multiple locations at once.
+
+            creation: new Date().toUTCString(),
+            */
+        };
+
+        database.set(newContact.id, newContact);
+    },
+};
 
 module.exports = repository;

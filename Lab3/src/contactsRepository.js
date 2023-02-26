882c1f20d226215229f86abfbff3222902fa899c
Aleks <akamenovic@hawk.iit.edu>
2023-02-26 12:53:52 -0600

    Refactor findAll to use SQLite database

--- Lab3/src/contactsRepository.js
+++ Lab3/src/contactsRepository.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const sqlite = require('better-sqlite3');
+const Contact = require('./Contact')
 
 const db = new sqlite(path.join(__dirname, '../database/contacts.sqlite'), {verbose: console.log});
 
@@ -10,7 +11,16 @@ const createTable = db.prepare("CREATE TABLE IF NOT EXISTS CONTACTS (id INTEGER 
 createTable.run();
 
 const repository = {
-    findAll: () => Array.from(database.values()),
+    findAll: () => {
+        const statement = db.prepare("SELECT * FROM CONTACTS");
+        const rows = statement.all();
+        let contacts = [];
+        rows.forEach((row) => {
+            const contact = new Contact(row.id, row.first_name, row.last_name, row.email, row.notes, row.creation, row.modified);
+            contacts.push(contact);
+        });
+        return contacts;
+    },
     findByID: (id) => database.get(id),
     create: (contacts) => {
         const newContact = {

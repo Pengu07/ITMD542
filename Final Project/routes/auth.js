c246de91a181ccf446b76dac3308a4fa80975d71
Aleksandar Kamenovic <akamenovic@hawk.iit.edu>
2023-04-21 12:48:23 -0500

    Add sign up page

--- Final Project/routes/auth.js
+++ Final Project/routes/auth.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET login page */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'login' });
+    res.render('login', { title: 'login' });
+});
+
+router.get('/signup', function(req, res, next) {
+    res.render('signup', { title: 'signup' });
 });
 
 module.exports = router;
